ad99f5da983a73914e13fbc7cc3de12600264e70
Nigel R. Pugh <nigel.pugh32@gmail.com>
2016-09-12 13:25:46 -0400

    adding the CSC412 directory

---
+++ CSC412/f.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+
+
+cout << " Hello World!, My name is Nigel R. Pugh\n";
+cout << " THis is the second line of my program ";
+
+
+return 0;
+
+
+
+
+
+}
